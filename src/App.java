c9bab50b3757db986e12bf61c400192f5bf86f63
Sergey Platonov <platserg94@yahoo.com>
2021-04-17 16:54:56 +0200

    initial commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+
+
+
+class App {
+    public static void main(String[] args) {
+        // Open file and extract the distance matrix ArrayList<ArrayList<Double>>
+
+        System.out.println("It works");
+    }
+}
