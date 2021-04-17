1ee4bf1586a6fe0d707de579ac967654aa33239d
Sergey Platonov <platserg94@yahoo.com>
2021-04-17 17:02:00 +0200

    created filereader

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 
 
 
-class App {
+public class App {
     public static void main(String[] args) {
         // Open file and extract the distance matrix ArrayList<ArrayList<Double>>
 
---
+++ src/FileDistanceReader.java
@@ -0,0 +1,10 @@
+
+
+import java.util.ArrayList;
+
+public class FileDistanceReader {
+    
+    public static ArrayList<ArrayList<Double>> extractFromFile(String path) {
+        throw new UnsupportedOperationException();
+    }
+}
